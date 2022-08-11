397aa3cb86d22699c9d813e41865015fdac94215
Abdal Shakir <64357110+abdalshakir@users.noreply.github.com>
2022-08-11 23:10:47 +0500

    completed result page

--- public/app.js
+++ public/app.js
@@ -1,153 +1,145 @@
-var min = 5;
-var sec = 0;
-var milisec = 0;
-var second = document.getElementById("sec")
-var minute = document.getElementById("min")
-var timer;
-
-function count() {
-    milisec++
-    if (milisec == 100) {
-        sec--;
-        milisec = 0;
-        second.innerHTML = sec;
-    } else if (min > 0 && sec == 0) {
-        min--;
-        sec = 59;
-        second.innerHTML = sec;
-        minute.innerHTML = min;
-    } else if (min == 0 && sec == 0) {
-        result()
-        // clearInterval(countdown)
-        sec = 00;
-        min = 00;
-        sec.innerHTML = sec;
-        minute.innerHTML = min;
-    }
-}
-timer = setInterval(count, 10)
-
-
 var questionsArray = [
     {
-        question: "Where is the correct place to insert a JavaScript?",
-        option1: "The <body> section",
-        option2: "Both the <head> section and the <body> section are correct",
-        option3: "The <head> section",
-        answer: 2,
+        //q1//
+        question: "Javascript is an _______ language?",
+        a: "Object Oriented",
+        b: "Object Based",
+        c: "None of above",
+        answer: "a"
     },
+    //q2//
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        option1: "link",
-        option2: "script",
-        option3: "h1",
-        answer: 2,
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        a: "var",
+        b: "let",
+        c: "Both A and B",
+        answer: "c"
     },
+    //q3//
     {
-        question: "The external JavaScript file must contain the <script> tag.",
-        option1: "False",
-        option2: "True",
-        option3: "None of the above",
-        answer: 2,
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        a: "getElementbyId()",
+        b: "getElementByClassName()",
+        c: "Both a and b",
+        answer: "c"
     },
+    //q4//
     {
-        question: 'How do you write "Hello World" in an alert box?',
-        option1: 'alert("Hello World");',
-        option2: 'msg("Hello World");',
-        option3: 'msgBox("Hello World");',
-        answer: 1,
+        question: "How can a datatype be declared to be a constant type?",
+        a: "var",
+        b: "constant",
+        c: "let",
+        answer: "b"
     },
+    //q5//
     {
-        question: "How do you create a function in JavaScript?",
-        option1: "function=myFunction()",
-        option2: "function:myFunction()",
-        option3: "function myFunction()",
-        answer: 3,
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        a: "document.write()",
+        b: "console.log()",
+        c: "All of the above",
+        answer: "c"
     },
+    //q6//
     {
-        question: 'How do you call a function named "myFunction"?',
-        option1: "call function myFunction()",
-        option2: "myFunction()",
-        option3: "call myFunction()",
-        answer: 2,
+        question: "What keyword is used to check whether a given property is valid or not",
+        a: "in",
+        b: "is in",
+        c: "exists",
+        answer: "a"
     },
+    //q7//
     {
-        question: "How to write an IF statement in JavaScript?",
-        option1: "if i = 5",
-        option2: "if (i == 5)",
-        option3: "if (i == 5) then",
-        answer: 2,
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        a: "Boolean",
+        b: "Undefined",
+        c: "Object",
+        answer: "c"
     },
+    //q8//
     {
-        question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
-        option1: "if (i != 5)",
-        option2: "if i =! 5 then",
-        option3: "if i <> 5",
-        answer: 1,
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        a: "Stringify()",
+        b: "Parse()",
+        c: "Convert()",
+        answer: "a",
     },
+    //q9//
     {
-        question: "How does a FOR loop start?",
-        option1: "for (i = 0; i <= 5)",
-        option2: "for i = 1 to 5",
-        option3: "for (i = 0; i <= 5; i++)",
-        answer: 3,
+        question: "Which of the following are closures in Javascript?",
+        a: "Variables",
+        b: "Function or Object",
+        c: "All of the above",
+        answer: "c"
     },
+    //q10//
     {
-        question: "How can you add a comment in a JavaScript?",
-        option1: '"This is comment',
-        option2: "<--This is comment-->",
-        option3: "// This is comment",
-        answer: 3,
+        question: "How to stop an interval timer in Javascript",
+        a: "clearInterval",
+        b: "clearTimer",
+        c: "intervalOver",
+        answer: "a"
     }
+
 ]
 
-var countDown = 0;
+var result = document.getElementById("main-card");
+var optionsAll = document.querySelectorAll(".options");
+var question = document.getElementById("question");
+var optionA = document.getElementById("opt1");
+var optionB = document.getElementById("opt2");
+var optionC = document.getElementById("opt3");
+var nextBtn = document.getElementById("next_btn");
+
+
 var queCount = 0;
+var score = 0;
+
+showQuestion(queCount);
 
 function showQuestion(a) {
-    var que = document.getElementById("que").innerHTML = questionsArray[a].question;
-    var option1 = document.getElementById("option1").innerHTML = questionsArray[a].option1;
-    var option2 = document.getElementById("option2").innerHTML = questionsArray[a].option2;
-    var option3 = document.getElementById("option3").innerHTML = questionsArray[a].option3;
+
+    deselectRadio();
+
+    var currentQuiz = questionsArray[queCount];
+
+    question.innerText = currentQuiz.question;
+    optionA.innerText = currentQuiz.a;
+    optionB.innerText = currentQuiz.b;
+    optionC.innerText = currentQuiz.c;
+
+    document.getElementById("queCount").innerText = queCount + 1;
 }
 
-function nextQuestion() {
-    if (countDown < questionsArray.length - 1) {
-        countDown++;
-        showQuestion(countDown);
+function deselectRadio() {
+    optionsAll.forEach(optionsAll => optionsAll.checked = false)
+}
+
+function getSelectedRadio() {
+    var answer;
+    optionsAll.forEach(optionsAll => {
+        if (optionsAll.checked) {
+            answer = optionsAll.id
+        }
+    })
+    console.log(answer)
+    return answer;
+}
+
+nextBtn.addEventListener("click", () => {
+    var correctAns = getSelectedRadio();
+    if (correctAns) {
+        if (correctAns === questionsArray[queCount].answer) {
+            score++;
+        }
         queCount++;
-        document.getElementById("queNum").innerHTML = queCount;
-        // checkAns();
+        if (queCount < questionsArray.length) {
+            showQuestion(queCount);
+        } else {
+            result.innerHTML = `
+            <div class="card-body" style="height: 250px !important;font-size: 25px;">
+            <h2>You answered ${score}/${questionsArray.length} questions correctly</h2>
+            </div>
+            `
+        }
     }
-}
-function backQuestion() {
-    queCount--;
-    document.getElementById("queNum").innerHTML = queCount;
-    showQuestion(queCount);
-
-}
-
-
-var score = 0;
-var correctAns = 0;
-var checkValue = document.getElementsByClassName("que-option");
-// function checkAns() {
-//     for (var i = 0; i <= checkValue.length; i++){
-//         if(checkValue[i].checked === questionsArray[i].answer){
-//             score += 10;
-//             correctAns++;
-//             checkValue[i].checked = false;
-//         }else if (checkValue[i] !== questionsArray[i].answer){
-//             checkValue[i].checked = false;
-//         }
-
-//     }
-// }
-console.log(checkValue.checked)
-
-
-window.onload = function () {
-    showQuestion(countDown);
-    queCount++;
-    document.getElementById("queNum").innerHTML = queCount;
-}
+})
